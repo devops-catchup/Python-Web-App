862f5b01783ef3f912d03fd0b2b25b0ba703184c
devops-catchup <165940879+devops-catchup@users.noreply.github.com>
2025-05-28 22:22:48 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -32,4 +32,7 @@ def thankyou():
     return render_template('thankyou.html', name=name, method=method, coupon=coupon)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.jinja_env.auto_reload = True
+    app.config["TEMPLATES_AUTO_RELOAD"] = True
+    app.run(host="0.0.0.0", port=port)
