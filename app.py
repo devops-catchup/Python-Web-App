d19950ed85375d5404e87ea34f302ea0e73423c7
Ubuntu <ubuntu@ip-172-31-88-75.ec2.internal>
2025-05-28 13:41:27 +0000

    msg

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+@app.route('/payment', methods=['GET', 'POST'])
+def payment():
+    if request.method == 'POST':
+        name = request.form['name']
+        method = request.form['payment']
+        coupon = request.form['coupon']
+        return redirect(url_for('thankyou', name=name, method=method, coupon=coupon))
+    return render_template('payment.html')
+
+@app.route('/thankyou')
+def thankyou():
+    name = request.args.get('name')
+    method = request.args.get('method')
+    coupon = request.args.get('coupon')
+    return render_template('thankyou.html', name=name, method=method, coupon=coupon)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
