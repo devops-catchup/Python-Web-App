5ec4bf8ca0358449f7dcb1f06ec361bfc0fa21ab
devops-catchup <165940879+devops-catchup@users.noreply.github.com>
2025-05-28 22:17:23 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -32,4 +32,4 @@ def thankyou():
     return render_template('thankyou.html', name=name, method=method, coupon=coupon)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
